8f04b60d2e036b6ac87eec1dd032ac8f9dbe56e2
Marijn van Vliet <w.m.vanvliet@gmail.com>
2017-07-25 09:24:06 +0300

    update links

--- make_links.sh
+++ make_links.sh
@@ -6,8 +6,4 @@ rm -rf ~/.vim
 ln -s ~/cfg/vim ~/.vim
 rm -f ~/.vimrc
 ln ~/.vim/vimrc ~/.vimrc
-rm -rf ~/.ipython
-ln -s ~/cfg/ipython ~/.ipython
-rm -rf ~/.config/matplotlib
 mkdir -p ~/.config
-ln -s ~/cfg/matplotlib ~/.config/matplotlib
